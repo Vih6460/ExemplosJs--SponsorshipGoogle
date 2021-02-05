af509e7d4ba01d17e1ad3891452df768a679c4de
Vih6460 <viniciuz6460@gmail.com>
2021-02-05 11:22:26 -0300

    ex14

--- CursoVideoJS/ex14/script.js
+++ CursoVideoJS/ex14/script.js
@@ -1,1 +1,19 @@
-alert("Olá")
+function verifica() {
+    let foto = document.querySelector("#imagem");
+    var hora = document.querySelector("#inputHora").value;
+
+    if (hora >=6 && hora < 12) {
+        foto.src = "./images/manha.png";
+        document.body.style.backgroundColor = "#cddce6";
+
+    }else if (hora < 18) {
+        foto.src = "./images/tarde.png";
+        document.body.style.backgroundColor = "#6a4b36";
+    }else {
+        foto.src = "./images/noite.png";
+        document.body.style.backgroundColor = "#1f212a";
+    }
+
+    document.querySelector("#inputHora").textContent = "";
+    alert(hora);
+}
